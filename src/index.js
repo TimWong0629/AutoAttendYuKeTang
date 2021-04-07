78164e87c3de216c44b5e14f07b059793ddd9616
TimWong0629 <52022940+TimWong0629@users.noreply.github.com>
2021-04-07 15:12:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -72,7 +72,7 @@ const execCheckIn = async () => {
   const lessonInfo = await getOnLessonInfo()
 
   if (!lessonInfo && count < times) {
-    setTimeout(execCheckIn, 1000 * 60 * 1)
+    setTimeout(execCheckIn, 60000)
     return
   } else if (count === times) {
     sendNotify('YuKeTang: fail', 'Not Found Online Class')
